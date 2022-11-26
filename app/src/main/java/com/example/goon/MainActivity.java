b9410ba3d269892ca4f551dbd4ce5449ede85e9c
Alcides Leguia <nathan@Alcidess-MacBook-Pro.local>
2022-11-25 22:37:54 -0500

    v1.0

--- app/src/main/java/com/example/goon/MainActivity.java
+++ app/src/main/java/com/example/goon/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.goon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.goon.views.HomeActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setReferences();
+    }
+
+    private void setReferences() {
+        imgMain=findViewById(R.id.imgMain);
+        imgMain.setOnClickListener(this::onClick);
+    }
+
+    private void onClick(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
