60c8a1472615ec74fe642f211816f9e6544363e9
Alcides Leguia <u201824041@upc.edu.pe>
2022-11-26 20:09:47 -0500

    v1.01

--- app/src/main/java/com/example/goon/views/HomeActivity.java
+++ app/src/main/java/com/example/goon/views/HomeActivity.java
@@ -3,15 +3,34 @@ package com.example.goon.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.goon.R;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button btnUser, btnDriver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        setReferences();
         //
+    }
+    private void setReferences(){
+        btnUser=findViewById(R.id.btnHomeUser);
+        btnDriver=findViewById(R.id.btnHomeDriver);
+        btnUser.setOnClickListener(this::onClick);
+        btnDriver.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SearchTripActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void onClick(View view) {
+        Intent intent = new Intent(this, SearchTripActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/goon/views/SearchTripActivity.java
@@ -0,0 +1,15 @@
+package com.example.goon.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.goon.R;
+
+import android.os.Bundle;
+
+public class SearchTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_trip);
+    }
+}
