239a9cb0fd343a2aea1a9df9ae1fd66a2340fe6a
Alcides Leguia <u201824041@upc.edu.pe>
2022-11-28 23:30:24 -0500

    v1.02

---
+++ app/src/main/java/com/example/goon/models/Trips.java
@@ -0,0 +1,64 @@
+package com.example.goon.models;
+
+import java.util.Date;
+
+public class Trips {
+
+    private String uid;
+    private String route;
+    private double price;
+    private Date date;
+    private String departure;
+    private String arrival;
+
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+
+}
--- app/src/main/java/com/example/goon/views/SearchTripActivity.java
+++ app/src/main/java/com/example/goon/views/SearchTripActivity.java
@@ -1,15 +1,44 @@
 package com.example.goon.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.goon.R;
+import com.example.goon.models.Trips;
+
 
 import android.os.Bundle;
 
-public class SearchTripActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SearchTripActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    List<Trips> tripsList= new ArrayList<>();
+    AdapterSearchTrip adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_trip);
+        setData();
+        //initializeFirebase();
+        showData();
+    }
+
+    private void showData() {
+        Trips t = new Trips();
+        t.setArrival("dddddd");
+        t.setRoute("d turasas");
+         tripsList.add(t);
+
+         adapter = new AdapterSearchTrip(SearchTripActivity.this,tripsList);
+         recyclerView.setAdapter(adapter);
+         recyclerView.setLayoutManager(new LinearLayoutManager(SearchTripActivity.this));
+    }
+
+    private void setData() {
+        recyclerView= findViewById(R.id.rvTrips);
     }
 }
